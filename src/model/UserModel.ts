38384cc17d22677640296e7522bb348f552ce89c
PasinduMshan <madushan01722@gmail.com>
2025-02-20 23:34:27 +0530

    create UserModel.ts

---
+++ src/model/UserModel.ts
@@ -0,0 +1,10 @@
+export default class UserModel {
+    userEmail: string;
+    userPassword: string;
+    userRole: string;
+    constructor(userEmail: string, userPassword: string, userRole: string) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userRole = userRole;
+    }
+}
