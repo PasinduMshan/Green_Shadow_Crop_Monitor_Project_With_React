833d24d888be1b0eef2c5b5dedf1eaa93f510bea
PasinduMshan <madushan01722@gmail.com>
2025-02-21 00:19:24 +0530

    create DashboardModel.ts

---
+++ src/model/DashboardModel.ts
@@ -0,0 +1,12 @@
+export declare class DashboardModel {
+    staffCount: string;
+    vehicleCount: string;
+    equipmentCount: string;
+    fieldCount: string;
+    constructor(staffCount: string, vehicleCount: string, equipmentCount: string, fieldCount: string) {
+        this.staffCount = staffCount;
+        this.vehicleCount = vehicleCount;
+        this.equipmentCount = equipmentCount;
+        this.fieldCount = fieldCount;
+    }
+}
